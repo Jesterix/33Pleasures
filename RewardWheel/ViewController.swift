8add5e6b33ff0f0f3972d93d18530cde62eb81a1
Jesterix <ghaidenko@gmail.com>
2019-06-22 14:39:31 +0300

    Initial Commit

---
+++ RewardWheel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RewardWheel
+//
+//  Created by Georgy Khaydenko on 22/06/2019.
+//  Copyright © 2019 Georgy Khaydenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
